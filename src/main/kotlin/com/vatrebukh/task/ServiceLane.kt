2e9125e7dcf9cf86015d4280e312e03ede763b53
Valerii Trebukh <Valerii.Trebukh@gmail.com>
2022-08-14 05:05:22 +0300

    Task: Service Lane

---
+++ src/main/kotlin/com/vatrebukh/task/ServiceLane.kt
@@ -0,0 +1,35 @@
+package com.vatrebukh.task
+
+/*
+ * Complete the 'serviceLane' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY cases
+ */
+
+fun serviceLane(widths: Array<Int>, cases: Array<Array<Int>>): Array<Int> {
+
+
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val n = first_multiple_input[0].toInt()
+
+    val t = first_multiple_input[1].toInt()
+
+    val width = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val cases = Array(t) { Array(2){ 0 } }
+
+    for (i in 0 until t) {
+        cases[i] = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    }
+
+    val result = serviceLane(width, cases)
+
+    println(result.joinToString("\n"))
+}
